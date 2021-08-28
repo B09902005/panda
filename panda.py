20e80caa60d75160329dedc536f506c6dedbd8b4
B09902005 <b09902005@ntu.edu.tw>
2021-08-28 17:42:44 +0800

    second commit

--- panda.py
+++ panda.py
@@ -2,14 +2,38 @@ import pandas
 
 data = pandas.Series([88,82,71,91,98])
 print(data,'\n')
+print('type: ',data.dtype)
+print('size: ',data.size)
+print('index: ',data.index, '\n')
+
+# if numbers:   max, min, nlargest(3), nsmallest(2), sum, prod, median, mean, std
+data = pandas.Series([88,82,71,91,98])
 print('max: ',data.max())
 print('min: ',data.min())
 print('median: ',data.median())
-print('mean: ',data.mean(),'\n')
-print(data * 1.1 + 5)
+print('mean: ',data.mean())
+print('std: ',data.std())
+print('data[2]: ',data[2],'\n')
+
+print(data * 1.1 + 5, '\n')
+
 
 print('\n\n\n')
 
+
+# if strings:  lower, upper, len, cat(sep = ','), contains("P"), replace("P","p")
+data = pandas.Series(['wuwuboss','snow','ppm','yousus','ac'])
+print(data, '\n')
+print(data.str.upper(), '\n')
+print(data.str.len(),'\n')
+print(data.str.cat(sep = ' , '),'\n')
+print(data.str.replace('wuwuboss','loser'),'\n')
+print(data.str.replace('wuwuboss','loser').str.cat(sep = '_'),'\n')
+
+
+
+
+'''
 data = pandas.DataFrame({'name':['wuwuboss','snow','ppm','yousus','ac'],
                          'color':['purple','blue','white','yellow','orange'],
                          'number':[28,29,19,17,9],
@@ -17,3 +41,4 @@ data = pandas.DataFrame({'name':['wuwuboss','snow','ppm','yousus','ac'],
 print(data,'\n')
 print(data['number'],'\n')  # show number
 print(data.iloc[1],'\n')    # show person
+'''
